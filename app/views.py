d4cfe02565288061ad3a657b8ec73a9923512611
szym3ns <horbalszymon@gmail.com>
2020-06-12 09:47:56 +0200

    Removed authentication

--- app/views.py
+++ app/views.py
@@ -12,34 +12,28 @@ from django.utils import timezone
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all().order_by('-date_joined')
     serializer_class = UserSerializer
-    permission_classes = [permissions.IsAuthenticated]
 
 
 class ReportTypesViewSet(viewsets.ModelViewSet):
     queryset = ReportType.objects.all().order_by('-name')
     serializer_class = ReportTypeSerializer
-    permission_classes = [permissions.IsAuthenticated]
 
 
 class ReportsViewSet(viewsets.ModelViewSet):
     queryset = Report.objects.all().order_by('-created')
     serializer_class = ReportSerializer
-    permission_classes = [permissions.IsAuthenticated]
 
 
 class ProjectsViewSet(viewsets.ModelViewSet):
     queryset = Project.objects.all().order_by('-name')
     serializer_class = ProjectSerializer
-    permission_classes = [permissions.IsAuthenticated]
 
 
 class NotificationsViewSet(viewsets.ModelViewSet):
     queryset = Notification.objects.all()
     serializer_class = NotificationSerializer
-    permission_classes = [permissions.IsAuthenticated]
 
 
 class NotificationStatusesViewSet(viewsets.ModelViewSet):
     queryset = NotificationStatus.objects.all().order_by('-name')
     serializer_class = NotificationStatusSerializer
-    permission_classes = [permissions.IsAuthenticated]
